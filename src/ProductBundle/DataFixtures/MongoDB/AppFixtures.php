2db6c7fbebc42f12bebdb3015b33a2211531a760
Lukasz Malicki <lukasz.malicki@polcode.com>
2015-07-15 15:55:02 +0200

    working mongodb

--- src/ProductBundle/DataFixtures/MongoDB/AppFixtures.php
+++ src/ProductBundle/DataFixtures/MongoDB/AppFixtures.php
@@ -1,51 +1,62 @@
-<?php
+<?php namespace ProductBundle\DataFixtures\ORM;
 
-namespace ProductBundle\DataFixtures\ORM;
-
-use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
-use Application\Sonata\UserBundle\Document\User;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+//use Application\Sonata\UserBundle\Document\User;
 
-Class AppFixtures extends AbstractFixture {
+Class AppFixtures implements FixtureInterface, ContainerAwareInterface
+{
+
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
 
     /**
      * {@inheritDoc}
      */
-    public function load(ObjectManager $manager) {
-        $this->manager = $manager;
-        
-        $this->loadAdminUser();
-        $this->loadSampleUsers();
-        
-    }
-
-    private function loadAdminUser() {
-        $userAdmin = new User();
-        $userAdmin->setUsername('admin');
-        $userAdmin->setEmail('email@example.com');
-        $userAdmin->setEnabled(true);
-        $userAdmin->addRole('ROLE_SONATA_ADMIN');
-        $userAdmin->setPlainPassword('admin');
-        
-        $this->manager->persist($userAdmin);
-        $this->manager->flush();
-    }
-
-    private function loadSampleUsers() {
-
-        for ($i = 1; $i <= 10; $i++) {
-            $user = new User();
-            $user->setUsername("user_$i");
-            $user->setEmail("sample$i@example.com");
-            $user->setEnabled(true);
-            $user->addRole('ROLE_USER');
-            $user->setPlainPassword("user_$i");
-            
-            $this->manager->persist($user);
-            $this->manager->flush();
-        }
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
     }
     
+    public function load(ObjectManager $manager)
+    {
+        $this->manager = $manager;
 
+        $this->loadAdminUser();
+        $this->loadSampleUsers();
+    }
 
+    private function loadAdminUser()
+    {
+        $userManager = $this->container->get('fos_user.user_manager');
+        // Create our user and set details
+        $user = $userManager->createUser();
+        $user->setUsername('root');
+        $user->setEmail('email@domain.com');
+        $user->setPlainPassword('polcode');
+        $user->setEnabled(true);
+        $user->setRoles(array('ROLE_SONATA_ADMIN'));
+        $userManager->updateUser($user, true);
+        
+    }
+
+    private function loadSampleUsers()
+    {
+        $userManager = $this->container->get('fos_user.user_manager');
+        
+        for ($i = 1; $i <= 10; $i++) {
+        // Create our user and set details
+        $user = $userManager->createUser();
+        $user->setUsername("user_$i");
+        $user->setEmail("sample$i@example.com");
+        $user->setPlainPassword("user_$i");
+        $user->setEnabled(true);
+        $user->setRoles(array('ROLE_USER'));
+        $userManager->updateUser($user, true);
+        }
+    }
 }
